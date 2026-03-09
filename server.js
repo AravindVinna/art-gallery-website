48da452806f91e3c28efa4206212f9f445d6b251
Aravindvinna <aravindvinnna123@gmail.com>
2026-03-09 18:41:35 +0530

    fix server port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -35,6 +36,8 @@ app.post("/login",(req,res)=>{
     }
 });
 
-app.listen(3000,()=>{
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
 });
